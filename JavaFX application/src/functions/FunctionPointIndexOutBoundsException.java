fd79f37a6a2dcdc80d534ecf8a81268f704ec0a8
l1can13 <78080932+l1can13@users.noreply.github.com>
2022-01-24 18:09:12 +0400

    Delete JavaFX application directory

--- JavaFX application/src/functions/FunctionPointIndexOutBoundsException.java
+++
@@ -1,8 +0,0 @@
-package functions;
-
-public class FunctionPointIndexOutBoundsException extends IndexOutOfBoundsException{
-
-    public FunctionPointIndexOutBoundsException(){super();}
-
-    public FunctionPointIndexOutBoundsException(String s){super(s);}
-}
